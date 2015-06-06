d4e1f3a02accb9d4dc4f5cf60e4ebed7e404250f
mambax7 <mambax7@gmail.com>
2015-06-06 07:54:13 -0400

    Original files

---
+++ ixt01705_v30/language/german/script.js
@@ -0,0 +1,14 @@
+/*
+* Qualitative Themes for XOOPS
+*
+* @license       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @project       IXThemes Project, http://ixthemes.org
+* @theme_name    ixt01705
+* @theme_release 3.0
+* @theme_demo    http://ixthemes.sourceforge.net/demo/x/index.php?xoops_theme_select=ixt01705
+* @theme_author  Alexander Galochkin (algalochkin at users.sourceforge.net)
+* @version       $Id: script.js 2769 2010-04-25 07:48:56Z algalochkin $
+*
+* Thanks you, that you have left this header untouched!!!
+*/
+/* Localization script */
